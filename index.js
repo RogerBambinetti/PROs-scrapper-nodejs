7cd514c5d79ef0aaef9fc8e2a103bef4481b1576
Roger <rogerbambinetti@hotmail.com>
2023-10-08 17:38:46 -0300

    remove logs

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const utils = require('./utils/util');
 async function init() {
     const data = await ascap.getFormattedData();
 
-    await utils.writeFile('log', data);
+    const currentDate = new Date();
+    const formattedDate = currentDate.getFullYear() + '.' + (currentDate.getMonth() + 1) + '.' + currentDate.getDate();
+
+    await utils.writeFile(`${formattedDate}`, data);
 
     process.exit();
 }
