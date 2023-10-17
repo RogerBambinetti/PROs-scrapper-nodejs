e5ae22ce97412160a32188c1ffa422f3e9d9496a
Roger <rogerbambinetti@hotmail.com>
2023-10-17 14:43:25 -0300

    read old data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { parse } = require('csv-parse/sync');
 const utils = require('./utils/util');
 
 async function init() {
@@ -17,15 +18,17 @@ async function init() {
 
         const csv = fs.readFileSync(`./logs/${moduleName}.csv`);
 
+        const oldData = parse(csv, {columns: true, delimiter: ';'});
         const data = await module.getFormattedData();
 
         for (const d of data) {
             if (!csv.toString().includes(d.workId)) {
+                oldData.push(d);
                 console.log('New record', d);
             }
         }
 
-        await utils.writeFile(moduleName, data)
+        await utils.writeFile(moduleName, oldData)
     };
 
     process.exit();
