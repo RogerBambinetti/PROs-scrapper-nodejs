9127fa14e9a27a654a2ac4d5c9789b72390a75fa
Roger <rogerbambinetti@hotmail.com>
2023-10-17 00:08:41 -0300

    new record log

--- index.js
+++ index.js
@@ -7,14 +7,25 @@ async function init() {
     const currentDate = new Date();
     const formattedDate = currentDate.getFullYear() + '.' + (currentDate.getMonth() + 1) + '.' + currentDate.getDate();
 
+    console.log('Init', formattedDate);
+
     const sources = fs.readdirSync('./sources');
 
     for (const source of sources) {
+        const moduleName = `${path.basename(source, '.js')}`;
         const module = require(`./sources/${source}`);
+
+        const csv = fs.readFileSync(`./logs/${moduleName}.csv`);
 
         const data = await module.getFormattedData();
 
-        await utils.writeFile(`${path.basename(source, '.js')} ${formattedDate}`, data)
+        for (const d of data) {
+            if (!csv.toString().includes(d.workId)) {
+                console.log('New record', d);
+            }
+        }
+
+        await utils.writeFile(moduleName, data)
     };
 
     process.exit();
