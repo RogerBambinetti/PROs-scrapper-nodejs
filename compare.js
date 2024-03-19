8e6a89f1a8383fbd08f4a6f0b4798403652db860
Roger <rogerbambinetti@hotmail.com>
2024-03-18 23:43:31 -0300

    adding readfile

---
+++ compare.js
@@ -0,0 +1,19 @@
+const util = require('./utils/util');
+
+async function init(){
+    const komca = await util.readFile('./logs/komca.csv');
+    const ascap = await util.readFile('./logs/ascap.csv');
+
+    const string = JSON.stringify(ascap);
+
+    komca.map(k => {
+        if(!string.includes(k.title)){
+            console.log(k.title)
+        }
+    })
+
+    console.log(file)
+}
+
+init()
+
--- utils/util.js
+++ utils/util.js
@@ -5,6 +5,14 @@ async function addDelay(seconds = 2) {
     return new Promise((resolve) => setTimeout(resolve, 1000 * seconds));
 }
 
+async function readFile(path){
+    const data = fs.readFileSync(path, {encoding: 'utf-8'});
+
+    const json = await jsoncsv.csv2json(data, {delimiter: {field: ';'}});
+
+    return json;
+}
+
 async function writeFile(title, data) {
     const csv = await jsoncsv.json2csv(data, {delimiter: {field: ';'}});
 
@@ -12,4 +20,4 @@ async function writeFile(title, data) {
     console.log('Wrote file to disk');
 }
 
-module.exports = { addDelay, writeFile };
+module.exports = { addDelay, readFile, writeFile };
